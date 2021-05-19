6d07a575b83c3f4abd8ac111f317947d842462d5
Topp <stopp@usgs.gov>
2021-05-19 15:14:17 -0400

    Updated My_happ_R code to be broken into three distinct scripts that can be sourced and will output information on relevant output files.

---
+++ 2_munge/src/1_PlotPrep_and_Summary.R
@@ -0,0 +1,59 @@
+##This script munges our Mendota data to prep it for Figure 1 and then
+##outputs a text summary of the results
+
+library(dplyr)
+library(readr)
+library(stringr)
+library(whisker)
+library(readr)
+
+## Function for plotting the data and comparing models
+munge_data <- function(fileIn, project_output_dir, name){
+  
+  if(dir.exists(project_output_dir) == F){
+    dir.create(project_output_dir)
+  }
+  
+  evals <- readr::read_csv(fileIn, col_types = 'iccd') %>%
+    filter(str_detect(exper_id, 'similar_[0-9]+')) %>%
+    mutate(col = case_when(
+      model_type == 'pb' ~ '#1b9e77',
+      model_type == 'dl' ~'#d95f02',
+      model_type == 'pgdl' ~ '#7570b3'
+    ), pch = case_when(
+      model_type == 'pb' ~ 21,
+      model_type == 'dl' ~ 22,
+      model_type == 'pgdl' ~ 23
+    ), n_prof = as.numeric(str_extract(exper_id, '[0-9]+')))
+  
+  readr::write_csv(evals, file = file.path(project_output_dir, name))
+  print(paste0("Munged eval data has been saved to ", file.path(project_output_dir, name)))
+  return(evals)
+}
+
+evals <- munge_data("1_fetch/out/me_RMSE.csv", '2_munge/out', 'model_summary_results.csv')
+
+## Summary text output
+
+summaryOut <- function(eval_data, project_output_dir, name){
+  render_data <- list(pgdl_980mean = filter(eval_data, model_type == 'pgdl', exper_id == "similar_980") %>% pull(rmse) %>% mean %>% round(2),
+                      dl_980mean = filter(eval_data, model_type == 'dl', exper_id == "similar_980") %>% pull(rmse) %>% mean %>% round(2),
+                      pb_980mean = filter(eval_data, model_type == 'pb', exper_id == "similar_980") %>% pull(rmse) %>% mean %>% round(2),
+                      dl_500mean = filter(eval_data, model_type == 'dl', exper_id == "similar_500") %>% pull(rmse) %>% mean %>% round(2),
+                      pb_500mean = filter(eval_data, model_type == 'pb', exper_id == "similar_500") %>% pull(rmse) %>% mean %>% round(2),
+                      dl_100mean = filter(eval_data, model_type == 'dl', exper_id == "similar_100") %>% pull(rmse) %>% mean %>% round(2),
+                      pb_100mean = filter(eval_data, model_type == 'pb', exper_id == "similar_100") %>% pull(rmse) %>% mean %>% round(2),
+                      pgdl_2mean = filter(eval_data, model_type == 'pgdl', exper_id == "similar_2") %>% pull(rmse) %>% mean %>% round(2),
+                      pb_2mean = filter(eval_data, model_type == 'pb', exper_id == "similar_2") %>% pull(rmse) %>% mean %>% round(2))
+  
+  template_1 <- 'Models resulted in mean RMSEs (means calculated as average of RMSEs from the five dataset iterations) of {{pgdl_980mean}}, {{dl_980mean}}, and {{pb_980mean}}°C for the PGDL, DL, and PB models, respectively.
+    The relative performance of DL vs PB depended on the amount of training data. The accuracy of Lake Mendota temperature predictions from the DL was better than PB when trained on 500 profiles 
+    ({{dl_500mean}} and {{pb_500mean}}°C, respectively) or more, but worse than PB when training was reduced to 100 profiles ({{dl_100mean}} and {{pb_100mean}}°C respectively) or fewer.
+    The PGDL prediction accuracy was more robust compared to PB when only two profiles were provided for training ({{pgdl_2mean}} and {{pb_2mean}}°C, respectively).'
+  
+  text <- whisker.render(template_1 %>% str_remove_all('\n') %>% str_replace_all('  ', ' '), render_data) 
+  cat(text, file = file.path(project_output_dir, name))
+  print(paste0("The following summary has been saved to ",file.path(project_output_dir, name),":  ",text))
+  }  
+  
+summaryOut(evals, '2_munge/out', 'model_diagnostic_text.txt')
